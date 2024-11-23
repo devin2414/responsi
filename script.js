349693a322620ea936ad21ad32939d519e907086
devin2414 <dvinaocta0424@gmail.com>
2024-11-23 10:44:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+downloadButton.addEventListener("mousedown", () => {
+  downloadButton.style.transform = "scale(0.95)";
+});
+
+downloadButton.addEventListener("mouseup", () => {
+  downloadButton.style.transform = "scale(1)";
+});
